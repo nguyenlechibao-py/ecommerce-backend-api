33cd2923d33a7fd84c7759a7b1ed996dd547fe4c
tunv <nguyenvantu191298@gmail.com>
2021-02-11 01:44:03 +0700

    add admin auth routes

---
+++ routes/api-admin.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+// AUTH ROUTES
+Route::group(['prefix' => 'admin'], function () {
+    Route::post('/login', 'Admin\AuthController@login');
+});
+
+Route::group(['prefix' => 'admin', 'middleware' => ['auth:admins', 'auth:api']], function () {
+    Route::post('/logout', 'Admin\AuthController@logout');
+    Route::post('/profile', 'Admin\AuthController@profile');
+    Route::post('/register', 'Admin\AuthController@register');
+});
