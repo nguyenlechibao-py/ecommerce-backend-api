06c9775379b80adbd02a5fe199fadcdd7cf4f544
tunv <nguyenvantu191298@gmail.com>
2021-02-17 11:26:06 +0700

    bug: update fails

--- routes/api-admin.php
+++ routes/api-admin.php
@@ -30,8 +30,9 @@ Route::prefix('category')->middleware('auth:admins')->group(function () {
 });
 
 Route::get('/categories/{id}', 'CategoryController@show');
+Route::get('/categories', 'CategoryController@index');
 
-Route::group(['prefix' => 'categories', 'middleware'=> ['auth:admins']], function () {
+Route::group(['prefix' => 'categories', 'middleware' => ['auth:admins']], function () {
     Route::post('/add', 'CategoryController@store');
     Route::put('/update/{id}', 'CategoryController@update');
     Route::delete('/delete/{id}', 'CategoryController@destroy');
