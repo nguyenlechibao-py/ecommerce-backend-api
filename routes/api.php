df1bc6d3d688d1f2211be0c5641301e5824e7806
tunv <nguyenvantu191298@gmail.com>
2021-02-21 08:48:55 +0700

    bug when delete image

--- routes/api.php
+++ routes/api.php
@@ -17,9 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::middleware(['auth:admins'])->prefix('media')->group(function () {
-    Route::post('/upload-new-media', 'Admin\MediaController@store');
-});
-
-Route::get('/media', 'Admin\MediaController@index');
