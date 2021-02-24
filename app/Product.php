b3dff04770beb4b3bf38389519061c8bcbfcb48e
akashicrecords-py <monkeyd.lucy2110@gmail.com>
2021-02-24 17:53:51 +0700

    fix quantity product

--- app/Product.php
+++ app/Product.php
@@ -11,7 +11,7 @@ class Product extends Model
 {
     protected $fillable = ['name', 'price', 'description', 'media_id', 'tag_id', 'category_id', 'quantity'];
 
-    protected $casts = ['category_id' => 'integer', 'media_id' => 'integer', 'tag_id' => 'array'];
+    protected $casts = ['category_id' => 'integer', 'media_id' => 'integer', 'quantity'=> 'integer', 'tag_id' => 'array'];
 
     public function category() {
         return $this->belongsTo(Category::class);
